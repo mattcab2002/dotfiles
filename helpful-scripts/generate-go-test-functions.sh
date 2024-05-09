6937e8f8b6ab31b8d182843f6dddd8e760a6a02c
Matthew Cabral <matthewanthonycabral@gmail.com>
2024-05-09 15:13:19 -0400

    feat: helpful bash scripts dir

---
+++ helpful-scripts/generate-go-test-functions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ $# < 2 ]]; then
+	echo "Usage: ./script.sh source target"
+	exit 1
+fi
+
+for FILE in $(grep -oP 'func\s+\K[A-Z]\w+' $1); do echo "func Test$FILE(t *testing.T){}" >>$2; done
